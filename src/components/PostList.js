0999361fa4a260094e809a26ddbb1dfcd6927d56
higer <higer@gmail.com>
2019-10-07 15:11:12 +1100

    render posts

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -5,9 +5,23 @@ class PostList extends Component {
   componentDidMount() {
     this.props.requestPosts();
   }
+
+  renderList = () => {
+    if (this.props.posts.data)
+      return this.props.posts.data.map(item => (
+        <div key={item.id} class="item">
+          <i class="user icon"></i>
+          <div class="content">
+            <a class="header">{item.title}</a>
+            <div class="description">{item.body}</div>
+          </div>
+        </div>
+      ));
+    else return <i class="spinner icon"></i>;
+  };
   render() {
-    console.log(this.props.posts);
-    return <div>PostList</div>;
+    this.renderList();
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
 
