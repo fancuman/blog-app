8573db48d776ecb56224797961efe27feb44acf5
higer <higer@gmail.com>
2019-10-07 19:20:35 +1100

    use lodash to avoid fetch the same user

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,21 @@
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
 import { FETCH_POSTS, FETCH_USER } from "./types";
+import _ from "lodash";
 
 export const requestPosts = () => async dispatch => {
   const response = await jsonPlaceHolder.get("/posts");
   dispatch({ type: FETCH_POSTS, payload: response });
 };
 
-export const fetchUser = id => async dispatch => {
+// export const fetchUser = id => async dispatch => {
+//   const response = await jsonPlaceHolder.get("/users/" + id);
+//   dispatch({ type: FETCH_USER, payload: response.data });
+// };
+
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceHolder.get("/users/" + id);
   dispatch({ type: FETCH_USER, payload: response.data });
-};
+});
 
 export default {};
